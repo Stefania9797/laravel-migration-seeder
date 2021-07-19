cf8767b4b040157733b2d6e2b8e7e90e16db8bb2
Stefania9797 <stefaniamastrel@gmail.com>
2021-07-19 15:26:07 +0200

    complete seeding

--- database/seeds/PackageSeeder.php
+++ database/seeds/PackageSeeder.php
@@ -1,5 +1,6 @@
 <?php
-
+use Faker\Generator as Faker;
+use App\Package;
 use Illuminate\Database\Seeder;
 
 class PackageSeeder extends Seeder
@@ -9,8 +10,17 @@ class PackageSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) { 
+            $package = new Package();
+            $package->city = $faker->city();
+            $package->description = $faker->paragraph();
+            $package->destination_img = $faker->imageUrl(360, 360, 'destination', true,);
+            $package->days = $faker->numberBetween(7, 21);
+            $package->price = $faker->numberBetween(500, 3000);
+            $package->date = $faker->dateTimeBetween('-1 week', '+10 week');
+            $package->save();
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $packages = App\Package::all();
+    return view('welcome',compact('packages'));
 });
