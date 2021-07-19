8290d4424eecc4326873cdb9b663581910a98275
Stefania9797 <stefaniamastrel@gmail.com>
2021-07-19 15:46:25 +0200

    add basic css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,23 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
+        <title>Pacchetti</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     </head>
     <body>
-        <h1>Ciao</h1>
+        <div class="packages">
+            @foreach ($packages as $package)
+            <div class="package">
+                <h1>{{$package->city}}</h1>
+                <img src="{{$package->destination_img}}" alt="">
+                <p>{{$package->description}}</p>
+                <h5><strong>Date:</strong> {{$package->date}}</h5>
+                <h5><strong>Days:</strong> {{$package->days}}</h5>
+                <h5><strong>Price:</strong> {{$package->price}} EURO</h5>
+            </div>
+            @endforeach
+        </div>
     </body>
 </html>
